88960d06183113a450ce324a3d4c2b0546eff5e0
Soobin Kittredge <soobin.kittredge@guildeducation.com>
2020-07-07 12:03:24 -0600

    modal automation

--- src/gameState.js
+++ src/gameState.js
@@ -1,4 +1,4 @@
-import { modFox, modScene, togglePoopBag } from "./ui";
+import { modFox, modScene, togglePoopBag, writeModal } from "./ui";
 import {
   RAIN_CHANCE,
   SCENES,
@@ -46,6 +46,7 @@ const gameState = {
     this.wakeTime = this.clock + 3;
     modFox("egg");
     modScene("day");
+    writeModal();
   },
   wake() {
     this.current = "IDLING";
