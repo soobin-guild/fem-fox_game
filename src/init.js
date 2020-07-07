7808ae0b2968bef913fef4e2e72f2ef5723b1c27
Soobin Kittredge <soobin.kittredge@guildeducation.com>
2020-07-07 09:57:14 -0600

    buildingg UI: HTML

--- src/init.js
+++ src/init.js
@@ -2,10 +2,6 @@ import gameState from "./gameState";
 
 const TICK_RATE = 3000;
 
-function tick() {
-  console.log("tick", Date.now());
-}
-
 async function init() {
   console.log("starting game");
 
@@ -15,7 +11,7 @@ async function init() {
     const now = Date.now();
 
     if (nextTimeToTick <= now) {
-      tick();
+      gameState.tick();
       nextTimeToTick = now + TICK_RATE;
     }
 
